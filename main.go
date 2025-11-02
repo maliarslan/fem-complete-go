fa4d89d36244838f5326cb09f22898265dd57c3e
maliarslan <muhenthis@gmail.com>
2025-11-03 00:55:41 +0100

    health check api

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/maliarslan/fem-complete-go/internal/app"
+)
+
+func main() {
+	app, err := app.NewApplication()
+
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Println("Running the application!")
+
+	http.HandleFunc("/health", HealthCheck)
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
 }
